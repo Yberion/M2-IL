89b6bd64f96c0c9df23379bb705cbf112116e6e8
Brandon <yberion.ins@gmail.com>
2020-09-22 19:47:31 +0200

    [S1 - GLI] TP2 - Initialisation du projet, Step 1, Step 2, Q1, Q2, Q3

---
+++ S1/GLI/tp2/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MyComponentComponent } from './my-component/my-component.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MyComponentComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ S1/GLI/tp2/src/app/my-component/my-component.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-component',
+  templateUrl: './my-component.component.html',
+  styleUrls: ['./my-component.component.css']
+})
+export class MyComponentComponent implements OnInit {
+  id: string;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
